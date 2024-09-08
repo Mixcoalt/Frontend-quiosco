d51ac9247f3d49577ed97c38010c31d689c90c90
JhovanPalafox <palafoxjhovan0@gmail.com>
2024-09-08 17:00:28 -0600

    cambios api

--- src/config/axios.js
+++ src/config/axios.js
@@ -4,7 +4,7 @@ import axios from "axios"
 const clienteAxios = axios.create({
     baseURL: import.meta.env.VITE_API_URL,
     headers: {
-        'Accept' : 'aplication/json',
+        'Accept' : 'application/json',
         'X-Requested-With' : 'XMLHttpRequest'
     },
     withCredentials: true
